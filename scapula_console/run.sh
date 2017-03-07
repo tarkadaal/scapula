37482c7f3a1bffa3f429849dc220613a7c9e96b5
Dan Jackson <danjacksonis@googlemail.com>
2017-03-07 14:19:37 +0000

    run script now hides console text

--- scapula_console/run.sh
+++ scapula_console/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #tvservice -o
 #tvservice -p
+export PS1=""
+clear
 while true; do
 	for FILENAME in $(ls -t1 *.mp4 *.MOV *.jpg)
 	do
 		if [ ${file: -4} == ".jpg" ]]
 		then
-			fbi -t 5 -1 -noverbose $FILENAME
+			fbi -t 5 -1 -noverbose $FILENAME &> /dev/null
 		else
-			omxplayer --blank $FILENAME
+			omxplayer --blank $FILENAME &> /dev/null
 		fi
 	done
 done
